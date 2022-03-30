50086ab98bc2426dd82f4621a7f5831553359d18
Joseph Luk <joseph.sc.luk@gmail.com>
2022-03-30 15:30:50 -0700

    Add custom Licon

--- src/components/MRFs.vue
+++ src/components/MRFs.vue
@@ -2,61 +2,15 @@
   <div style="height: 75vh; width: 100%;">
     <l-map v-model="zoom" v-model:zoom="zoom" :center="[33.5265212, -86.777367]" @move="log('move')">
       <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
-      <l-control-layers />
       <l-marker :lat-lng="[0, 0]" draggable @moveend="log('moveend')">
         <l-tooltip>lol</l-tooltip>
       </l-marker>
-
-      <l-marker :lat-lng="[47.41322, -1.219482]">
-        <l-icon :icon-url="iconUrl" :icon-size="iconSize" />
-      </l-marker>
-      <l-marker :lat-lng="[33.5265212, -86.777367]">
+      <l-marker :lat-lng="[33.5265212, -86.777367]" :icon="icon">
         <l-popup>MRF: Birmingham Recycling and Recovery</l-popup>
       </l-marker>
-
       <l-marker :lat-lng="[50, 50]" draggable @moveend="log('moveend')">
         <l-popup>lol</l-popup>
       </l-marker>
-
-      <l-polyline
-        :lat-lngs="[
-          [47.334852, -1.509485],
-          [47.342596, -1.328731],
-          [47.241487, -1.190568],
-          [47.234787, -1.358337],
-        ]"
-        color="green"
-      ></l-polyline>
-      <l-polygon
-        :lat-lngs="[
-          [46.334852, -1.509485],
-          [46.342596, -1.328731],
-          [46.241487, -1.190568],
-          [46.234787, -1.358337],
-        ]"
-        color="#41b782"
-        :fill="true"
-        :fillOpacity="0.5"
-        fillColor="#41b782"
-      />
-      <l-rectangle
-        :lat-lngs="[
-          [46.334852, -1.509485],
-          [46.342596, -1.328731],
-          [46.241487, -1.190568],
-          [46.234787, -1.358337],
-        ]"
-        :fill="true"
-        color="#35495d"
-      />
-      <l-rectangle
-        :bounds="[
-          [46.334852, -1.190568],
-          [46.241487, -1.090357],
-        ]"
-      >
-        <l-popup>lol</l-popup>
-      </l-rectangle>
     </l-map>
   </div>
 </template>
@@ -74,6 +28,7 @@ import {
   LRectangle,
 } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
+import { icon } from 'leaflet'
 
 export default {
   components: {
@@ -93,6 +48,14 @@ export default {
       zoom: 3,
       iconWidth: 25,
       iconHeight: 40,
+      icon: icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      })
     };
   },
   computed: {
