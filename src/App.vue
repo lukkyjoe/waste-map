f92bc395bc63db5efc217e6787a66559f19587ae
Joseph Luk <joseph.sc.luk@gmail.com>
2022-03-30 16:40:26 -0700

    Add v-checkbox

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,20 @@
   <v-app>
     <v-main>
       <MRFs/>
+      <v-checkbox
+        v-model="ex4"
+        label="MRFs"
+        color="green"
+        value="red"
+      >
+      </v-checkbox>
+      <v-checkbox
+        v-model="ex5"
+        label="manufacturers"
+        color="blue"
+        value="red"
+      >
+      </v-checkbox>
     </v-main>
   </v-app>
 </template>
